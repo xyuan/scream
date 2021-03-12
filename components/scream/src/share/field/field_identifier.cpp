9c011425fbcf06776d9b1dd66a26702933c03119
Luca Bertagna <lbertag@sandia.gov>
2021-03-11 19:02:39 -0700

    SCREAM: upgrades to FieldIdentifier
    The class now stores also exposes a long name, while 'name' is now
    referring to a short and handy name. The long name does not contribute
    to the id string, so there *could* be two instances of this class that
    have all members equal except the long name, and the op== would return
    true. The FieldRepository will be the class most in charge of making
    sure that this doesn't happen.
    
    Furthermore, the class now uses a sort-of pimpl idiom to store the data
    (the data struct is actually not hidden completely (it is in the header
    file). The reason is to limit duplication of the same data around,
    since FieldIdentifier's are quite commonly stored around classes,
    such as atm processes, remappers, I/O, and more.

--- components/scream/src/share/field/field_identifier.cpp
+++ components/scream/src/share/field/field_identifier.cpp
@@ -8,11 +8,19 @@ FieldIdentifier (const std::string& name,
                  const layout_type& layout,
                  const units_type& units,
                  const std::string& grid_name)
- : m_name      (name)
- , m_units     (units)
- , m_grid_name (grid_name)
+ : FieldIdentifier(name,"",layout,units,grid_name)
 {
-  set_layout (layout);
+  // Nothing to do here
+}
+
+FieldIdentifier::
+FieldIdentifier (const std::string& name,
+                 const std::string& long_name,
+                 const layout_type& layout,
+                 const units_type&  units,
+                 const std::string& grid_name)
+{
+  m_data = std::make_shared<Data>(name,long_name,layout,units,grid_name);
 }
 
 void FieldIdentifier::set_layout (const layout_type& layout) {
@@ -20,29 +28,36 @@ void FieldIdentifier::set_layout (const layout_type& layout) {
 }
 
 void FieldIdentifier::set_layout (const layout_ptr_type& layout) {
-  EKAT_REQUIRE_MSG (!m_layout,
+  EKAT_REQUIRE_MSG (!m_data->layout,
       "Error! You cannot reset the layout once it's set.\n");
   EKAT_REQUIRE_MSG (layout,
       "Error! Invalid input layout pointer.\n");
   EKAT_REQUIRE_MSG (layout->are_dimensions_set(),
       "Error! Input layout must have dimensions set.\n");
 
-  m_layout = layout;
+  m_data->layout = layout;
+}
+
+void FieldIdentifier::set_long_name (const std::string& long_name) {
+  EKAT_REQUIRE_MSG (m_data->long_name=="",
+      "Error! You cannot change the long name once it has been set.\n");
+
+  m_data->long_name = long_name;
 }
 
 std::string FieldIdentifier::get_id_string () const {
   // Create a verbose identifier string.
-  std::string id = m_name + "[" + m_grid_name + "]";
-  if (m_layout) {
-    id += "<" + e2str(m_layout->tags()[0]);
-    for (int dim=1; dim<m_layout->rank(); ++dim) {
-      id += "," + e2str(m_layout->tags()[dim]);
+  std::string id = m_data->name + "[" + m_data->grid_name + "]";
+  if (m_data->layout) {
+    id += "<" + e2str(m_data->layout->tags()[0]);
+    for (int dim=1; dim<m_data->layout->rank(); ++dim) {
+      id += "," + e2str(m_data->layout->tags()[dim]);
     }
-    id += ">(" + std::to_string(m_layout->dims()[0]);
-    for (int dim=1; dim<m_layout->rank(); ++dim) {
-      id += "," + std::to_string(m_layout->dims()[dim]);
+    id += ">(" + std::to_string(m_data->layout->dims()[0]);
+    for (int dim=1; dim<m_data->layout->rank(); ++dim) {
+      id += "," + std::to_string(m_data->layout->dims()[dim]);
     }
-    id += ") [" + m_units.get_string() + "]";
+    id += ") [" + m_data->units.get_string() + "]";
   }
   return id;
 }
--- components/scream/src/share/field/field_identifier.hpp
+++ components/scream/src/share/field/field_identifier.hpp
@@ -13,10 +13,20 @@ namespace scream
 /*
  *  A small class to hold basic info about a field
  *
- *  This tiny class is only used to uniquely identify a field, using
- *  the name, and its layout (which containss the rank, the tags,
- *  and possibly the dimensions).
- *  There is no additional meta data about this field.
+ *  The FieldIdentifier (FID) is used to uniquely identify a field, using
+ *  the name, its layout (tags and dimensions), the name of the grid
+ *  where the field is defined, and its units.
+ *  The FID stores 2 names: a short one (m_name) and a long one (m_long_name).
+ *  The latter is built according to CF names guidelines:
+ *    http://cfconventions.org/Data/cf-standard-names/docs/guidelines.html
+ *  The user only needs to pass a name at construction time, but
+ *  can also provide the long CF-compliant one (if known).
+ *  When field are registered in the FieldRepo (FR), we check that both
+ *  short and long names are unique, meaning that
+ *   - the same short name is not used for two different long names;
+ *   - the same long name is not used for two different short names.
+ *  The FR will also take care of ensuring that a valid long name
+ *  is stored.
  */
 
 class FieldIdentifier {
@@ -34,17 +44,24 @@ public:
                    const units_type& units,
                    const std::string& grid_name);
 
+  FieldIdentifier (const std::string& name,
+                   const std::string& long_name,
+                   const layout_type& layout,
+                   const units_type& units,
+                   const std::string& grid_name);
+
   // Delete assignment, to prevent overwriting identifiers sneakyly
   FieldIdentifier& operator= (const FieldIdentifier&) = delete;
 
   // ----- Getters ----- //
 
   // Name and layout informations
-  const std::string&      name           () const { return  m_name;      }
-  const layout_type&      get_layout     () const { return *m_layout;    }
-  const layout_ptr_type&  get_layout_ptr () const { return  m_layout;    }
-  const units_type&       get_units      () const { return  m_units;     }
-  const std::string&      get_grid_name  () const { return  m_grid_name; }
+  const std::string&      name           () const { return  m_data->name;      }
+  const std::string&      long_name      () const { return  m_data->long_name; }
+  const layout_type&      get_layout     () const { return *m_data->layout;    }
+  const layout_ptr_type&  get_layout_ptr () const { return  m_data->layout;    }
+  const units_type&       get_units      () const { return  m_data->units;     }
+  const std::string&      get_grid_name  () const { return  m_data->grid_name; }
 
   // The identifier string is a conveniet way to display the information of
   // the identifier, so that it can be easily read.
@@ -56,23 +73,47 @@ public:
   void set_layout (const layout_type& layout);
   void set_layout (const layout_ptr_type& layout);
 
+  // Note: as soon as the long name is set, it cannot be changed
+  void set_long_name (const std::string& long_name);
+
   // We reimplement the equality operator for identifiers comparison (needed for some std container)
   friend bool operator== (const FieldIdentifier&, const FieldIdentifier&);
   friend bool operator<  (const FieldIdentifier&, const FieldIdentifier&);
 
 protected:
 
-  // The field name
-  ci_string       m_name;
+  struct Data {
+    Data (const std::string& _name,
+          const std::string& _long_name,
+          const layout_type& _layout,
+          const units_type&  _units,
+          const std::string& _grid_name)
+      : name(_name)
+      , long_name(_long_name)
+      , layout(std::make_shared<const layout_type>(_layout))
+      , units(_units)
+      , grid_name(_grid_name)
+    {
+      // Nothing to do here
+    }
 
-  // The layout of the field
-  layout_ptr_type m_layout;
+    // The field name
+    ci_string       name;
 
-  // The units of this field
-  units_type      m_units;
+    // An long_name, compliant with cf naming conventions
+    ci_string       long_name;
 
-  // The name of the grid on which the field is defined
-  ci_string       m_grid_name;
+    // The layout of the field
+    layout_ptr_type layout;
+
+    // The units of this field
+    units_type      units;
+
+    // The name of the grid on which the field is defined
+    ci_string       grid_name;
+  };
+
+  std::shared_ptr<Data> m_data;
 };
 
 bool operator== (const FieldIdentifier& fid1, const FieldIdentifier& fid2);
